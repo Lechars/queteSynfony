58379e9d88810b13449506e3b5ff19283e1d68c2
Charles <charles.chevreuil@gmail.com>
2020-05-15 12:23:26 +0200

    le commit de la quete 5

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,9 +7,16 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
-import '../css/test.css';
+//import '../css/test.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+const $ = require('jquery');
+require('bootstrap');
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,15 +6,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @Route("/wild", name="wild_")
+ */
+
 Class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="index")
      */
     public function index() : Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/show/{slug}",requirements={"slug"="([a-z0-9](-)?)+"}, defaults={"slug"="pageVide"}, name="show")
+     */
+    public function show(string $slug): Response
+    {
+        if ($slug == "pageVide") {
+            return $this->render('wild/show.html.twig', ['metalSlug' => "Aucune série sélectionnée, veuillez choisir une série"]);
+        } else {
+            $slug = str_replace("-", " ", $slug);
+            $slugTab = explode(" ", $slug);
+            foreach ($slugTab as $key => $limace) {
+                $slugTab[$key] = ucfirst($limace);
+            }
+            $slug = implode(" ", $slugTab);
+            return $this->render('wild/show.html.twig', ['metalSlug' => $slug]);
+        }
     }
 }
