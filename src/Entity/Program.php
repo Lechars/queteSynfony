ff60627ccdc56380a21d5800202755d1582e53f7
Charles <charles.chevreuil@gmail.com>
2020-05-31 14:49:21 +0200

    quete 13

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,6 +6,7 @@ use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProgramRepository")
@@ -21,6 +22,9 @@ class Program
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="ce champ vide!!!! noonnn!!!")
+     * @Assert\Length(max="255",maxMessage="ceci {{ value }} n'est pas bon {{ limit }}")
+     * @Assert\Unique
      */
     private $title;
 
@@ -31,6 +35,7 @@ class Program
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank(message="ce champ vide!!!! noonnn!!!")
      */
     private $poster;
 
