d5f6c148974a065e49316cce9f25f4164902a523
Charles <charles.chevreuil@gmail.com>
2020-06-09 21:27:30 +0200

    quete18

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -11,6 +11,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 
 /**
  * @Route("/program")
@@ -30,7 +34,7 @@ class ProgramController extends AbstractController
     /**
      * @Route("/new", name="program_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, MailerInterface $mailer): Response
     {
         $program = new Program();
         $form = $this->createForm(ProgramType::class, $program);
@@ -39,6 +43,13 @@ class ProgramController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($program);
+            $email = (new TemplatedEmail())
+                ->from('charles.chevreuil@gmail.com')
+                ->to('charles.chevreuil@gmail.com')
+                ->subject('Une nouvelle série vient d\'être publiée !')
+                ->htmlTemplate('emails/email.html.twig');
+
+            $mailer->send($email);
             $entityManager->flush();
 
             return $this->redirectToRoute('program_index');
